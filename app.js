f4506c339996a32d5dbdaf77a516916320e7b29a
Cynthia Henaff <cynthia.henaff@hotmail.fr>
2017-12-03 16:44:16 +0100

    Add MongoDb support

--- app.js
+++ app.js
@@ -1,44 +1,52 @@
 require('dotenv').config();
 
+const { MongoClient, ObjectID } = require('mongodb');
 const KrakenClient = require('kraken-api');
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+
+(async () => {
+  console.log('Bot is starting');
+
+  console.log('Connection to the database');
+  const db = await MongoClient.connect(process.env.MONGODB_URL);
 
 
-console.log('Bot is starting');
-const key          = process.env.KRAKEN_KEY; // API Key
-const secret       = process.env.KRAKEN_SECRET; // API Private Key
-const kraken       = new KrakenClient(key, secret, {
-  timeout: 30 * 1000
-});
-
-const chatId = -1001340775946;
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
-bot.start((ctx) => {
-  // chatId = ctx.chat.id;
-  console.log('started:', ctx.from.id);
-  console.log(ctx.from);
-  return ctx.reply(`Welcome ${ctx.from.first_name}!`);
-})
-bot.command('help', (ctx) => ctx.reply('/how - Query the market'))
-bot.command('how', (ctx) => {
-  ctx.reply('I\'m searching...');
-  getTickers();
-})
+  const key = process.env.KRAKEN_KEY; // API Key
+  const secret = process.env.KRAKEN_SECRET; // API Private Key
+  const kraken = new KrakenClient(key, secret, { timeout: 30 * 1000 });
 
 
-// bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
-// bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
-// bot.on('sticker', (ctx) => ctx.reply('👍'))
+  const chatId = -1001340775946;
+  const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+  bot.start(async (ctx) => {
+    // chatId = ctx.chat.id;
+    console.log('started:', ctx.from.id);
+    console.log(ctx.from);
+    await db.collection('users').insert(ctx.from);
 
-bot.startPolling()
+    return ctx.reply(`Welcome ${ctx.from.first_name}!`);
+  });
+  bot.command('help', (ctx) => ctx.reply('/how - Query the market'));
+  bot.command('how', (ctx) => {
+    ctx.reply('I\'m searching...');
+    getTickers();
+  });
+  // bot.on('new_chat_members', (ctx) => ctx.reply(`Hello ${ctx.from.first_name}`));
 
-const getTickers = async () => {
+  // bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
+  // bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
+  // bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+  bot.startPolling();
+
+
+  const getTickers = async () => {
     // Display user's balance
     // console.log(await kraken.api('Balance'));
 
     // Get Ticker Info
     const data = await kraken.api('Ticker', { pair : 'XXBTZEUR,XETHZEUR' });
-    const dataStringified = JSON.stringify(data);
+    // const dataStringified = JSON.stringify(data);
     // console.log(dataStringified);
 
     const ethereum = data.result.XETHZEUR.c[0];
@@ -53,6 +61,8 @@ const getTickers = async () => {
     if (chatId) {
       bot.telegram.sendMessage(chatId, `Bitcoin is at ${bitcoinFloat} euros\nEther is at ${ethereumFloat} euros` );
     }
-};
+  };
 
-setInterval(getTickers, 60 * 60 * 1000);
+  setInterval(getTickers, 60 * 60 * 1000);
+
+})();
