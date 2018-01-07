abd2d0a7a82ccc228a1ece1dbe503ab2f3fb3ffe
Cynthia Henaff <cynthia@henaff.io>
2018-01-07 19:50:37 +0100

    Replace Kraken API by the library ccxt and add new currencies

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 
+const ccxt = require('ccxt');
 const { MongoClient, ObjectID } = require('mongodb');
-const KrakenClient = require('kraken-api');
 const Telegraf = require('telegraf');
+
+const cryptos = [
+  {
+    symbol: 'BTC/EUR',
+    name: 'Bitcoin',
+    exchange: 'gdax',
+    command: 'bitcoin',
+  },
+  {
+    symbol: 'ETH/EUR',
+    name: 'Ethereum',
+    exchange: 'gdax',
+    command: 'ether',
+  },{
+    symbol: 'XRP/EUR',
+    name: 'Ripple',
+    exchange: 'kraken',
+    command: 'ripple',
+  },
+];
 
 (async () => {
   console.log('Bot is starting');
@@ -10,29 +30,58 @@ const Telegraf = require('telegraf');
   console.log('Connection to the database');
   const db = await MongoClient.connect(process.env.MONGODB_URL);
 
+  const fetchTicker = async (exchangeName, symbol) => {
+    const exchange = new ccxt[exchangeName]();
+    const ticker = await exchange.fetchTicker(symbol);
+    return ticker.last;
+  };
 
-  const key = process.env.KRAKEN_KEY; // API Key
-  const secret = process.env.KRAKEN_SECRET; // API Private Key
-  const kraken = new KrakenClient(key, secret, { timeout: 30 * 1000 });
-
-
-  const chatId = -1001340775946;
+  const channelId = '@ButterInTheSpinach';
   const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+
   bot.start(async (ctx) => {
-    // chatId = ctx.chat.id;
     console.log('started:', ctx.from.id);
     console.log(ctx.from);
     await db.collection('users').insert(ctx.from);
 
     return ctx.reply(`Welcome ${ctx.from.first_name}!`);
   });
-  bot.command('help', (ctx) => ctx.reply('/how - Query the market'));
-  bot.command('how', (ctx) => {
-    ctx.reply('I\'m searching...');
-    getTickers();
-  });
-  // bot.on('new_chat_members', (ctx) => ctx.reply(`Hello ${ctx.from.first_name}`));
 
+
+
+  bot.command('help', (ctx) => {
+    let message = '/howMuch - Query the market\n';
+    for (const crypto of cryptos) {
+      message = message + `/${crypto.command} - Amount of ${crypto.name} in Euro\n`;
+    }
+    ctx.reply(message);
+  });
+
+  bot.command('howMuch', async (ctx) => {
+    ctx.reply('I\'m searching...');
+    try {
+      const sentence = await getTickers();
+      ctx.reply(sentence);
+      // Shortest version:
+      // ctx.reply(getTickers());
+    }
+    catch (error) {
+      ctx.reply('Sorry there is an error. Please try again in a few minutes.');
+    }
+  });
+
+  for (const crypto of cryptos) {
+    bot.command(crypto.command, async (ctx) => {
+      try {
+        const lastValue = await fetchTicker(crypto.exchange, crypto.symbol);
+        ctx.reply(`${crypto.name} is at ${lastValue} euros on ${crypto.exchange}`);
+      }
+      catch (error) {
+        ctx.reply('Sorry there is an error. Please try again in a few minutes.');
+      }
+    });
+  }
+  // bot.on('new_chat_members', (ctx) => ctx.reply(`Hello ${ctx.from.first_name}`));
   // bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
   // bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
   // bot.on('sticker', (ctx) => ctx.reply('👍'))
@@ -41,28 +90,28 @@ const Telegraf = require('telegraf');
 
 
   const getTickers = async () => {
-    // Display user's balance
-    // console.log(await kraken.api('Balance'));
-
     // Get Ticker Info
-    const data = await kraken.api('Ticker', { pair : 'XXBTZEUR,XETHZEUR' });
-    // const dataStringified = JSON.stringify(data);
-    // console.log(dataStringified);
-
-    const ethereum = data.result.XETHZEUR.c[0];
-    const ethereumFloat = parseFloat(ethereum);
-    console.log('Ether is at ' + ethereumFloat + ' euros');
-
-    const bitcoin = data.result.XXBTZEUR.c[0];
-    const bitcoinFloat = parseFloat(bitcoin);
-    // String template ES6
-    console.log(`Bitcoin is at ${bitcoinFloat} euros`);
-
-    if (chatId) {
-      bot.telegram.sendMessage(chatId, `Bitcoin is at ${bitcoinFloat} euros\nEther is at ${ethereumFloat} euros` );
+    try {
+      let message = '';
+      for (const crypto of cryptos) {
+        const lastValue = await fetchTicker(crypto.exchange, crypto.symbol);
+        // const lastBtc = await fetchTicker('gdax', 'BTC/EUR');
+        // const lastEth = await fetchTicker('gdax', 'ETH/EUR');
+        // const lastXrp = await fetchTicker('kraken', 'XRP/EUR');
+        message = message + `${crypto.name} is at ${lastValue} euros on ${crypto.exchange}\n`;
+      }
+      return message;
+    }
+    catch (error) {
+      return 'Sorry there is an error. Please try again in a few minutes.';
     }
   };
 
-  setInterval(getTickers, 60 * 60 * 1000);
+  const messageToChannel = async () => {
+    const sentence = await getTickers();
+    bot.telegram.sendMessage(channelId, sentence);
+  };
+
+  setInterval(messageToChannel, 60 * 60 * 1000);
 
 })();
