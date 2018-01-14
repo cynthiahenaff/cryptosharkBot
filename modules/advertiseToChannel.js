dd124dff5889b4204b8c89f8c70bea4f37a08fa4
Cynthia Henaff <cynthia@henaff.io>
2018-01-14 18:32:30 +0100

    Resolve a bug with advertise message to channel

--- modules/advertiseToChannel.js
+++ modules/advertiseToChannel.js
@@ -2,7 +2,8 @@ module.exports = (bot, channelId) => {
   const advertiseToChannel = async () => {
     const hours = new Date().getHours();
     const minutes = new Date().getMinutes();
-    if (hours !== 18 && minutes !== 0) {
+
+    if (!(hours === 18 && minutes === 0)) {
       return;
     }
     const message = 'Don\'t forget, you can talk directly with me by clicking on this link @ButterInTheSpinachBot 🤖 and join my mom at https://twitter.com/monsieur_riz';
