3091972212c9326c6ad0f35de8c89c385bda58d7
Cynthia Henaff <cynthia@henaff.io>
2018-01-29 18:05:03 +0100

    add commands in help command

--- modules/botCommandHelp.js
+++ modules/botCommandHelp.js
@@ -8,16 +8,17 @@ module.exports = (bot) => {
     for (const ticker of tickers.slice(0, 10)) {
       message = message + `/${ticker.symbol} - Value of ${ticker.name} in EUR/USD\n`;
     }
-    message +=  '\n*Best performing currencies*' +
-                '\n/best1h - Top 5 performing currencies in the last hour' +
-                '\n/best24h - Top 5 performing currencies in the last 24h' +
-                '\n/best7d - Top 5 performing currencies in the last week' +
-                '\n\n*Less performing currencies*' +
-                '\n/worst1h - 5 less performing currencies in the last hour' +
-                '\n/worst24h - 5 less performing currencies in the last 24h' +
-                '\n/worst7d - 5 less performing currencies in the last week' +
-                // '\n\n*More information*' +
-                // '\n/source';
+    message = message +
+      '\n*Best performing currencies*' +
+      '\n/best1h - Top 5 performing currencies in the last hour' +
+      '\n/best24h - Top 5 performing currencies in the last 24h' +
+      '\n/best7d - Top 5 performing currencies in the last week' +
+      '\n\n*Less performing currencies*' +
+      '\n/worst1h - 5 less performing currencies in the last hour' +
+      '\n/worst24h - 5 less performing currencies in the last 24h' +
+      '\n/worst7d - 5 less performing currencies in the last week';
+      // '\n\n*More information*' +
+      // '\n/source';
     ctx.replyWithMarkdown(message);
   });
 };
