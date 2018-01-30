7de40a0671f3641b2f3bd987fe14b3af87385a61
jules Ivanic <jules.ivanic@gmail.com>
2018-01-30 23:42:53 +0100

    Fix code

--- modules/botCommandBest7d.js
+++ modules/botCommandBest7d.js
@@ -5,25 +5,26 @@ module.exports = (bot) => {
   bot.command('best7d', (ctx) => {
     ctx.reply('I\'m searching...');
 
-    const takeFiveBestTickers /* List[Ticker] => List[Ticker] */ = myTickers =>
-        myTickers.
+    const takeFiveBestTickers /* List[Ticker] => List[Ticker] */ =
+      myTickers =>
+        myTickers
             .slice(0, 100)
             .sort((a, b) => parseFloat(b.percent_change_7d) - parseFloat(a.percent_change_7d))
-            .slice(0, 5);
+            .slice(0, 5)
 
     const bestForTicker /* Ticker => Promise[(Struct, Ticker)] */ =
-            bestCurrencie => fetchTicker(bestCurrencie.id).then(res => { res, bestCurrencie })
+        bestCurrencie => fetchTicker(bestCurrencie.id).then(res => { res, bestCurrencie })
 
     const bestsForTickers /* List[Ticker] => Promise[List[(Struct, Ticker)]] */ =
         bestCurrencies => Promise.all(bestCurrencies.map(bestForTicker))
 
-      const bestAsString /*: List[(Struct, Ticker)] => List[String] */ = (myStructs, best) =>
-        myStructs.map(s => `/${best.symbol} - ${best.name}\n\t*${s.changeOver7d}*%\n`)
+    const bestAsString /*: (Struct, Ticker) => List[String] */ =
+        (myStruct, best) => `/${best.symbol} - ${best.name}\n\t*${myStruct.changeOver7d}*%\n`
 
     fetchTickers() /*: Promise[List[Ticker]] */
       .then(takeFiveBestTickers)
       .then(bestsForTickers)
-      .then(bestAsString)
+      .then(bests => bests.map(bestAsString))
       .then(strings => strings.join('\n'))
       .then(message => ctx.replyWithMarkdown(message + '\n/help to see the others commands!'))
       .catch(_ => ctx.reply('Sorry there is an error. Please try again in a few minutes.'));
