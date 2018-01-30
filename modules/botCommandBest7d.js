a1bf2a28f830a40391a6f1e7515b52c85fa43cc3
jules Ivanic <jules.ivanic@gmail.com>
2018-01-30 21:49:16 +0100

    Functionnal programming <3

--- modules/botCommandBest7d.js
+++ modules/botCommandBest7d.js
@@ -2,28 +2,30 @@ const fetchTickers = require('./fetchTickers');
 const fetchTicker = require('./fetchTicker');
 
 module.exports = (bot) => {
-  bot.command('best7d', async (ctx) => {
+  bot.command('best7d', (ctx) => {
     ctx.reply('I\'m searching...');
-    const tickers = await fetchTickers();
-    let bestCurrencies = tickers
-      .slice(0, 100)
-      .sort((a, b) => { return parseFloat(b.percent_change_7d) - parseFloat(a.percent_change_7d); })
-      .slice(0, 5);
 
-    try {
-      let message = (await Promise.all(
-        bestCurrencies.map(
-          async (bestCurrencie) => {
-            const result = await fetchTicker(bestCurrencie.id);
-            return `/${bestCurrencie.symbol} - ${bestCurrencie.name}\n\t*${result.changeOver7d}*%\n`;
-          }
-        )
-      )).join('\n');
-      message += '\n/help to see the others commands!';
-      ctx.replyWithMarkdown(message);
-    }
-    catch (error) {
-      ctx.reply('Sorry there is an error. Please try again in a few minutes.');
-    }
+    const takeFiveBestTickers /* List[Ticker] => List[Ticker] */ = myTickers =>
+        myTickers.
+            .slice(0, 100)
+            .sort((a, b) => parseFloat(b.percent_change_7d) - parseFloat(a.percent_change_7d))
+            .slice(0, 5);
+
+    const bestForTicker /* Ticker => Promise[(Struct, Ticker)] */ =
+            bestCurrencie => fetchTicker(bestCurrencie.id).then(res => { res, bestCurrencie })
+
+    const bestsForTickers /* List[Ticker] => Promise[List[(Struct, Ticker)]] */ =
+        bestCurrencies => Promise.all(bestCurrencies.map(bestForTicker))
+
+      const bestAsString /*: List[(Struct, Ticker)] => List[String] */ = (myStructs, best) =>
+        myStructs.map(s => `/${best.symbol} - ${best.name}\n\t*${s.changeOver7d}*%\n`)
+
+    fetchTickers() /*: Promise[List[Ticker]] */
+      .then(takeFiveBestTickers)
+      .then(bestsForTickers)
+      .then(bestAsString)
+      .then(strings => strings.join('\n'))
+      .then(message => ctx.replyWithMarkdown(message + '\n/help to see the others commands!'))
+      .catch(_ => ctx.reply('Sorry there is an error. Please try again in a few minutes.'));
   });
 };
